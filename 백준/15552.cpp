4c8d3b728a1a7c872f5e1cf34d81c4d6068a1bf6
lys7aves <lys7aves@gmail.com>
2022-02-12 11:36:12 +0900

    feature: boj 15552

---
+++ 백준/15552.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t, a, b;
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> t;
+	while(t--){
+		cin >> a >> b;
+		cout << a+b << '\n';
+	}
+	
+	return 0;
+}
