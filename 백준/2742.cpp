b0c7e6425dbfc22d8937b28f7b2c5848d057f77e
lys7aves <lys7aves@gmail.com>
2022-02-18 22:38:12 +0900

    Study(boj): boj 2742

---
+++ 백준/2742.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n;
+	for(int i=n; i>0; i--) cout << i << '\n';
+	
+	return 0;
+}
