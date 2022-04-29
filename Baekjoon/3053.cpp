08e6444af363caa276a6a8593b512e5cdb5dfc23
lys7aves <lys7aves@gmail.com>
2022-04-29 23:39:23 +0900

    Study(Boj): Boj 3053

---
+++ Baekjoon/3053.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	double r;
+	
+	cin >> r;
+	
+	cout << fixed;
+	cout.precision(10);
+	
+	cout << r * r * M_PI << '\n';
+	cout << r * r * 2;
+	
+	return 0;
+}
