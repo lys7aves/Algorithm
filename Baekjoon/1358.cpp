9cdbe8ab35f5bddad1b147fabb80f9032a04cc02
lys7aves <lys7aves@gmail.com>
2022-05-18 16:58:22 +0900

    Study(Boj): Boj 1358

---
+++ Baekjoon/1358.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int w, h, x1, y1, x2, y2, r, p, x, y, dx, dy, cnt;
+	
+	cin >> w >> h >> x >> y >> p;
+	
+	r = h/2;
+	x1 = x;
+	y1 = y+r;
+	x2 = x1 + w;
+	y2 = y1;
+	
+	while(p--){
+		cin >> x >> y;
+		
+		dx = x - x1;
+		dy = y - y1;
+		if(dx*dx + dy*dy <= r*r){
+			cnt++;
+			continue;
+		}
+		
+		dx = x - x2;
+		dy = y - y2;
+		if(dx*dx + dy*dy <= r*r){
+			cnt++;
+			continue;
+		}
+		
+		if(x1 <= x && x <= x2 && abs(y1-y) <= r) cnt++;
+	}
+	
+	cout << cnt;
+	
+	return 0;
+}
