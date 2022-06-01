ec9f506e3c7c0f55af53de883604dcd963ae569a
lys7aves <lys7aves@gmail.com>
2022-06-02 07:14:56 +0900

    Study(Boj): Boj 15652

---
+++ Baekjoon/15652.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int ans[10];
+
+void dfs(int p, int q)
+{
+	int i;
+	
+	if(p == m){
+		for(i=0; i<m; i++) cout << ans[i] << ' ';
+		cout << '\n';
+		
+		return;
+	}
+	
+	for(i=q; i<=n; i++){
+		ans[p] = i;
+		
+		dfs(p+1, i);
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	
+	dfs(0, 1);
+	
+	return 0;
+}
