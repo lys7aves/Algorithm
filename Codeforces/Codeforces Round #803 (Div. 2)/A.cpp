f04b19ad30b34a8579b2435303b82d956ebf99c9
lys7aves <lys7aves@gmail.com>
2022-06-28 11:37:04 +0900

    Study(Codeforces): Codeforces Round #803 (Div. 2) - A

---
+++ Codeforces/Codeforces Round #803 (Div. 2)/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t, n, a;
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> t;
+	while(t--){
+		cin >> n;
+		while(n--) cin >> a;
+		
+		cout << a << '\n';
+	}
+	
+	return 0;
+}
